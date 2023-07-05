66c93d3f309a7e7bccadee17e39d8652e098711f
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-07-05 10:09:36 -0300

    second commit

--- config.js
+++ config.js
@@ -9,3 +9,16 @@ const empresas = [
 
 const exibeInformacoes = empresas.map( empresa => `${empresa.nome}` + ' CEO: ' + `${empresa.CEO}`);
 console.log( exibeInformacoes )
+
+const salario =[
+    {id: 1, valorHora: 10.00, horasMes: 200},
+    {id: 2, valorHora: 15.00, horasMes: 220},
+    {id: 3, valorHora: 12.00, horasMes: 240},
+    {id: 4, valorHora: 20.00, horasMes: 200},
+    {id: 5, valorHora: 30.00, horasMes: 220},
+];
+
+salario.forEach(element => console.log(element.id, element.horasMes));
+
+const salarioMes = salario.map(element => element.horasMes*element.valorHora)
+console.log(salarioMes)
